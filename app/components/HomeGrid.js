d211b7b2d21b38cb564f3b881e25bbcc2a27bf0d
Rishav Sharma <shalinisharmar60@gmail.com>
2024-10-17 00:18:08 +0530

    commit

---
+++ app/components/HomeGrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeGrid() {
+  return (
+    <div>HomeGrid</div>
+  )
+}
+
+export default HomeGrid
---
+++ app/constants.js
@@ -0,0 +1,9 @@
+import HomeGrid from "./components/HomeGrid"
+import ProjectsGrid from "./components/ProjectsGrid"
+
+export const GRIDS = [
+    { label: "home", Component: HomeGrid },
+
+    { label: "projects", Component: ProjectsGrid},
+    
+]
